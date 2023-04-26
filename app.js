f50e805bcf81cb59d7d83208bc2e87825f356e51
atsa21 <ts21ann@gmail.com>
2023-04-27 00:58:12 +0300

    mongo uri fix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const cors = require('cors');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
@@ -24,18 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join("backend/images")));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://archiprod-shop.web.app");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-  );
-  next();
-});
+app.use(cors({ origin: true }));
 
 app.use("/api/categories", categoriesRoutes);
 app.use("/api/brands", brandsRoutes);
