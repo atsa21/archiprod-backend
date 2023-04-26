bcf2771775dd90d17aca7df58e2ceb3e27cb6231
atsa21 <ts21ann@gmail.com>
2023-04-26 22:35:29 +0300

    change http to https

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const debug = require("debug")("node-angular");
-const http = require("http");
+const https = require("https");
 
 const normalizePort = val => {
   var port = parseInt(val, 10);
@@ -46,7 +46,7 @@ const onListening = () => {
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
-const server = http.createServer(app);
+const server = https.createServer(app);
 server.on("error", onError);
 server.on("listening", onListening);
 server.listen(port);
